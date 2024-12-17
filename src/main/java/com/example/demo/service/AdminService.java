4b4ae216e77c6bf602ff562143c40443e5a650c8
sooyeon_heo <suzi97@naver.com>
2024-12-17 16:09:34 +0900

    🩹 Fix : Lv.4 DB 접근 최소화

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -18,12 +18,19 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+        // 쿼리 한 번으로 모든 사용자 조회
+        List<User> users = userRepository.findAllById(userIds);
 
-            user.updateStatusToBlocked();
-
-            userRepository.save(user);
+        if (users.size() != userIds.size()) {
+            throw new IllegalArgumentException("일부 사용자 Id에 맞는 값이 존재하지 않습니다.");
         }
+
+        // 상태를 변경
+        users.forEach((User::updateStatusToBlocked));
+
+        // saveAll 을 호출하지 않아도 @Transactional 에 의해 변경사항 반영
+        // userRepository.saveAll(users); 필요시 명시적 호출
+
+        // 기존 문제: findById() 사용자 데이터 매번 조회, save() 매번 사용자 데이터 저장 -> 데이터 양이 많아질 수록 쿼리수가 (조회+저장)2*N
     }
 }
